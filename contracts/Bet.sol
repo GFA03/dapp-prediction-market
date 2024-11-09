4ec948af43f3d7356d54cb3df2cbb229f4170e87
GFA03 <florinalexandru302003@gmail.com>
2024-11-09 16:34:07 +0200

    add closing and canceling bet functionality, distribute rewards after declaring winner for bet and refactored tests

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.27;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Bet is Ownable{
+contract Bet is Ownable {
     enum Status {
         Open,
         Closed,
@@ -15,16 +15,28 @@ contract Bet is Ownable{
         uint amount;
     }
 
-    address public owner;
     Status public status;
     string public name;
     string[] public options;
     // mapping from adress to option index and amount
-    mapping (address => BetRecord) public bets;
+    mapping(address => BetRecord) public bets;
     address[] public bettors;
+    mapping(address => bool) public refunded; // Track refunded bettors
 
-    constructor (string memory _name, string[] memory _options) {
-        owner = msg.sender;
+    event BetEvent(
+        address indexed bettor,
+        uint indexed option,
+        uint indexed amount
+    );
+    event CloseEvent();
+    event CancelEvent();
+    event CashbackEvent(address indexed bettor, uint indexed amount);
+    event WinnerEvent(address indexed winner, uint indexed amount);
+
+    constructor(
+        string memory _name,
+        string[] memory _options
+    ) Ownable(msg.sender) {
         status = Status.Open;
         name = _name;
         options = _options;
@@ -37,14 +49,35 @@ contract Bet is Ownable{
     function bet(uint _option) public payable {
         require(status == Status.Open, "Betting is closed");
         require(_option < options.length, "Invalid option");
-        require(msg.value > 0, "Value must be greater than 0");
+        require(msg.value > 0, "Invalid amount");
         require(bets[msg.sender].amount == 0, "You already bet");
 
-        if (bets[msg.sender].amount == 0) {
-            bettors.push(msg.sender);
+        bets[msg.sender] = BetRecord(_option, msg.value);
+        bettors.push(msg.sender);
+
+        emit BetEvent(msg.sender, _option, msg.value);
+    }
+
+    function distributeRewards(uint _winningOption) public onlyOwner {
+        require(status == Status.Closed, "Betting must be closed to set a winner");
+        require(_winningOption < options.length, "Invalid option");
+        
+        uint totalWinningAmount = 0;
+
+        // Calculate total bet amount for the winning option
+        for (uint i = 0; i < bettors.length; i++) {
+            if (bets[bettors[i]].option == _winningOption) {
+                totalWinningAmount += bets[bettors[i]].amount;
+            }
         }
 
-        bets[msg.sender] = BetRecord(_option, msg.value);
+        // Distribute rewards to winners
+        for (uint i = 0; i < bettors.length; i++) {
+            if (bets[bettors[i]].option == _winningOption) {
+                uint payout = (bets[bettors[i]].amount * address(this).balance) / totalWinningAmount;
+                payable(bettors[i]).transfer(payout);
+            }
+        }
     }
 
     function getBet() public view returns (uint, uint) {
@@ -52,16 +85,36 @@ contract Bet is Ownable{
         return (betRecord.option, betRecord.amount);
     }
 
-    function close() public {
-        require(msg.sender == owner, "You aren't the owner");
+    function close() public onlyOwner {
+        require(status == Status.Open, "Betting is closed");
+        status = Status.Closed;
+        emit CloseEvent();
+    }
+
+    // function to return the bet amount to the bettor if the bet was canceled
+    function restoreFunds() private onlyOwner {
+        require(status == Status.Canceled, "Betting is not canceled");
+
+        for (uint i = 0; i < bettors.length; i++) {
+            address bettor = bettors[i];
+            BetRecord memory betRecord = bets[bettor];
+            payable(bettor).transfer(betRecord.amount);
+
+            emit CashbackEvent(bettor, betRecord.amount);
+        }
+    }
+
+    function cancel() public onlyOwner {
         require(status == Status.Open, "Betting is closed");
 
-        status = Status.Closed;
+        status = Status.Canceled;
+
+        restoreFunds();
+
+        emit CancelEvent();
     }
 
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
 }
-
-// todo: check for a bettor who cashed back (and remained in the bettors array) to not throw an error when searching in the bets mapping
--- test/Bet.ts
+++ test/Bet.ts
@@ -1,107 +1,85 @@
 import hre from "hardhat";
 import { expect } from "chai";
-import {
-    loadFixture,
-  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { Bet } from "../typechain-types";
 
-
 describe("Bet", () => {
+    const OPTION_INDEX = 0;
+    const AMOUNT = 1000;
+    const INVALID_OPTION_INDEX = 2;
+    const REVERT_BET_CLOSED = "Betting is closed";
+    const REVERT_ALREADY_BET = "You already bet";
+    const REVERT_INVALID_OPTION = "Invalid option";
+
     async function deployBet() {
         const name = 'Test Bet';
         const options = ['Option 1', 'Option 2'];
-
         const [owner, otherAccount] = await hre.ethers.getSigners();
-
         const bet = await hre.ethers.deployContract("Bet", [name, options]);
-
-        return {bet, name, options, owner, otherAccount};
+        return { bet, name, options, owner, otherAccount };
     }
 
     describe("Deployment", () => {
-        it("Should deploy correctly", async () => {
-            const { bet } = await loadFixture(deployBet);
-
-            const addr = await bet.getAddress();
-            expect(addr).to.properAddress;
-        });
-
-        it("Should set the right owner", async () => {
-            const { bet, owner } = await loadFixture(deployBet);
-
-            const actual = await bet.owner();
-            expect(actual).to.equal(owner.address);
-        });
-
-        it("Should set the right status", async () => {
-            const { bet } = await loadFixture(deployBet);
-
-            const actual = await bet.status();
-            // status equals 'Open' which is 0
-            expect(actual).to.equal(0);
-        });
-
-        it("Should set the right name", async () => {
-            const { bet, name } = await loadFixture(deployBet);
-            
-            const actual = await bet.name();
-            expect(actual).to.equal(name);
-        });
-
-        it("Should set the right options", async () => {
-            const { bet, options } = await loadFixture(deployBet);
-
-            const actual = await bet.getOptions();
-
-            expect(actual).to.be.an('array');
-            expect(actual).to.have.lengthOf(options.length);
-            expect(actual).to.deep.equal(options);
+        it("Should deploy correctly with the right address, owner, status, name, and options", async () => {
+            const { bet, name, options, owner } = await loadFixture(deployBet);
+            expect(await bet.getAddress()).to.properAddress;
+            expect(await bet.owner()).to.equal(owner.address);
+            expect(await bet.status()).to.equal(0);  // Status.Open is 0
+            expect(await bet.name()).to.equal(name);
+            expect(await bet.getOptions()).to.deep.equal(options);
         });
     });
 
     describe("Betting", () => {
         let bet: Bet, owner: HardhatEthersSigner, otherAccount: HardhatEthersSigner;
-        let optionIndex: number, amount: number;
 
         beforeEach(async () => {
             ({ bet, owner, otherAccount } = await loadFixture(deployBet));
-            optionIndex = 0;
-            amount = 1000;
         });
 
-        it("Should allow betting", async () => {
-            await bet.connect(otherAccount).bet(optionIndex, { value: amount });
+        async function placeBet(account: HardhatEthersSigner, option = OPTION_INDEX, amount = AMOUNT) {
+            return bet.connect(account).bet(option, { value: amount });
+        }
 
-            const actual = await bet.getBalance();
-            expect(actual).to.equal(amount);
+        it("Should allow a single bet and update the balance", async () => {
+            await placeBet(otherAccount);
+            expect(await bet.getBalance()).to.equal(AMOUNT);
         });
 
-        it("Should not allow betting multiple times", async () => {
-            const amount2 = 2000;
-            await bet.connect(otherAccount).bet(optionIndex, { value: amount });
-
-            await expect(bet.connect(otherAccount).bet(optionIndex, { value: amount2 })).to.be.revertedWith('You already bet');
+        it("Should not allow betting multiple times by the same account", async () => {
+            await placeBet(otherAccount);
+            await expect(placeBet(otherAccount)).to.be.revertedWith(REVERT_ALREADY_BET);
         });
 
-        it("Should not allow betting on different options", async () => {
-            const optionIndex2 = 1;
-            await bet.connect(otherAccount).bet(optionIndex, { value: amount });
-
-            await expect(bet.connect(otherAccount).bet(optionIndex2, { value: amount })).to.be.revertedWith('You already bet');
+        it("Should revert on multiple bets with different options", async () => {
+            await placeBet(otherAccount);
+            await expect(placeBet(otherAccount, 1)).to.be.revertedWith(REVERT_ALREADY_BET);
         });
 
-        it("Should not allow betting after closing", async () => {
+        it("Should revert if betting is closed", async () => {
             await bet.connect(owner).close();
-
-            await expect(bet.connect(otherAccount).bet(optionIndex, { value: amount }))
-                .to.be.revertedWith("Betting is closed");
+            await expect(placeBet(otherAccount)).to.be.revertedWith(REVERT_BET_CLOSED);
         });
 
-        it("Should not allow betting on invalid option", async () => {
-            const invalidOptionIndex = 2;
+        it("Should revert for invalid option", async () => {
+            await expect(placeBet(otherAccount, INVALID_OPTION_INDEX)).to.be.revertedWith(REVERT_INVALID_OPTION);
+        });
 
-            await expect(bet.connect(otherAccount).bet(invalidOptionIndex, { value: amount }))
-                .to.be.revertedWith("Invalid option");
+        it("Should allow different accounts to bet on the same option and accumulate balance", async () => {
+            await placeBet(otherAccount);
+            await placeBet(owner);
+            expect(await bet.getBalance()).to.equal(AMOUNT * 2);
+        });
+
+        it("Should allow different accounts to bet on different options and accumulate balance", async () => {
+            await placeBet(otherAccount);
+            await placeBet(owner, 1);
+            expect(await bet.getBalance()).to.equal(AMOUNT * 2);
+        });
+
+        it("Should revert bets after event is canceled", async () => {
+            await bet.connect(owner).cancel();
+            await expect(placeBet(otherAccount)).to.be.revertedWith(REVERT_BET_CLOSED);
         });
     });
 });
