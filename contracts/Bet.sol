63dd4d71f9fe9543da5c00c94bf2528f19790860
GFA03 <florinalexandru302003@gmail.com>
2024-11-09 12:14:54 +0200

    add name to bet contract

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -1,5 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
+import "hardhat/console.sol";
 
 contract Bet {
+    string public name;
+
+    constructor (string memory _name) {
+        name = _name;
+    }
 }
--- test/Bet.ts
+++ test/Bet.ts
@@ -1,15 +1,32 @@
 import hre from "hardhat";
 import { expect } from "chai";
+import {
+    loadFixture,
+  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 
 describe("Bet", () => {
+    async function deployBet() {
+        const name = 'Test Bet';
+
+        const bet = await hre.ethers.deployContract("Bet", [name]);
+
+        return {bet, name};
+    }
+
     describe("Deployment", () => {
         it("Should deploy correctly", async () => {
-            const Bet = await hre.ethers.getContractFactory("Bet");
-            const bet = await Bet.deploy();
-            
+            const { bet } = await loadFixture(deployBet);
+
             const addr = await bet.getAddress();
             expect(addr).to.properAddress;
         });
+
+        it("Should get the bet's name", async () => {
+            const { bet, name } = await loadFixture(deployBet);
+            
+            const actual = await bet.name();
+            expect(actual).to.equal(name);
+        })
     });
 });
