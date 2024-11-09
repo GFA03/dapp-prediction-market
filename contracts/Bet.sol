98e689e1dd814ed97201d9403f3b912e45552b28
GFA03 <florinalexandru302003@gmail.com>
2024-11-09 12:54:16 +0200

    add status

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -3,10 +3,20 @@ pragma solidity ^0.8.27;
 import "hardhat/console.sol";
 
 contract Bet {
+    enum Status {
+        Open,
+        Closed,
+        Canceled
+    }
+
+    address public owner;
+    Status public status;
     string public name;
     string[] public options;
 
     constructor (string memory _name, string[] memory _options) {
+        owner = msg.sender;
+        status = Status.Open;
         name = _name;
         options = _options;
     }
--- test/Bet.ts
+++ test/Bet.ts
@@ -10,9 +10,11 @@ describe("Bet", () => {
         const name = 'Test Bet';
         const options = ['Option 1', 'Option 2'];
 
+        const [owner, otherAccount] = await hre.ethers.getSigners();
+
         const bet = await hre.ethers.deployContract("Bet", [name, options]);
 
-        return {bet, name, options};
+        return {bet, name, options, owner, otherAccount};
     }
 
     describe("Deployment", () => {
@@ -23,19 +25,33 @@ describe("Bet", () => {
             expect(addr).to.properAddress;
         });
 
-        it("Should get the bet's name", async () => {
+        it("Should set the right owner", async () => {
+            const { bet, owner } = await loadFixture(deployBet);
+
+            const actual = await bet.owner();
+            expect(actual).to.equal(owner.address);
+        });
+
+        it("Should set the right status", async () => {
+            const { bet } = await loadFixture(deployBet);
+
+            const actual = await bet.status();
+            // status equals 'Open' which is 0
+            expect(actual).to.equal(0);
+        });
+
+        it("Should set the right name", async () => {
             const { bet, name } = await loadFixture(deployBet);
             
             const actual = await bet.name();
             expect(actual).to.equal(name);
         });
 
-        // it should get the bet's options
-        it("Should get the bet's options", async () => {
+        it("Should set the right options", async () => {
             const { bet, options } = await loadFixture(deployBet);
 
             const actual = await bet.getOptions();
-            
+
             expect(actual).to.be.an('array');
             expect(actual).to.have.lengthOf(options.length);
             expect(actual).to.deep.equal(options);
