9b1302c479dc3bbba1484bc7d9ccc2dec8a3ac72
GFA03 <florinalexandru302003@gmail.com>
2024-11-09 14:00:07 +0200

    add betting functionality

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -9,10 +9,18 @@ contract Bet {
         Canceled
     }
 
+    struct BetRecord {
+        uint option;
+        uint amount;
+    }
+
     address public owner;
     Status public status;
     string public name;
     string[] public options;
+    // mapping from adress to option index and amount
+    mapping (address => BetRecord) public bets;
+    address[] public bettors;
 
     constructor (string memory _name, string[] memory _options) {
         owner = msg.sender;
@@ -24,4 +32,35 @@ contract Bet {
     function getOptions() public view returns (string[] memory) {
         return options;
     }
+
+    function bet(uint _option) public payable {
+        require(status == Status.Open, "Betting is closed");
+        require(_option < options.length, "Invalid option");
+        require(msg.value > 0, "Value must be greater than 0");
+        require(bets[msg.sender].amount == 0, "You already bet");
+
+        if (bets[msg.sender].amount == 0) {
+            bettors.push(msg.sender);
+        }
+
+        bets[msg.sender] = BetRecord(_option, msg.value);
+    }
+
+    function getBet() public view returns (uint, uint) {
+        BetRecord memory betRecord = bets[msg.sender];
+        return (betRecord.option, betRecord.amount);
+    }
+
+    function close() public {
+        require(msg.sender == owner, "You aren't the owner");
+        require(status == Status.Open, "Betting is closed");
+
+        status = Status.Closed;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 }
+
+// todo: check for a bettor who cashed back (and remained in the bettors array) to not throw an error when searching in the bets mapping
--- test/Bet.ts
+++ test/Bet.ts
@@ -3,6 +3,7 @@ import { expect } from "chai";
 import {
     loadFixture,
   } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { Bet } from "../typechain-types";
 
 
 describe("Bet", () => {
@@ -56,6 +57,51 @@ describe("Bet", () => {
             expect(actual).to.have.lengthOf(options.length);
             expect(actual).to.deep.equal(options);
         });
+    });
 
+    describe("Betting", () => {
+        let bet: Bet, owner: HardhatEthersSigner, otherAccount: HardhatEthersSigner;
+        let optionIndex: number, amount: number;
+
+        beforeEach(async () => {
+            ({ bet, owner, otherAccount } = await loadFixture(deployBet));
+            optionIndex = 0;
+            amount = 1000;
+        });
+
+        it("Should allow betting", async () => {
+            await bet.connect(otherAccount).bet(optionIndex, { value: amount });
+
+            const actual = await bet.getBalance();
+            expect(actual).to.equal(amount);
+        });
+
+        it("Shouldn't be allowed betting multiple times", async () => {
+            const amount2 = 2000;
+            await bet.connect(otherAccount).bet(optionIndex, { value: amount });
+
+            await expect(bet.connect(otherAccount).bet(optionIndex, { value: amount2 })).to.be.revertedWith('You already bet');
+        });
+
+        it("Shouldn't be allowed betting on different options", async () => {
+            const optionIndex2 = 1;
+            await bet.connect(otherAccount).bet(optionIndex, { value: amount });
+
+            await expect(bet.connect(otherAccount).bet(optionIndex2, { value: amount })).to.be.revertedWith('You already bet');
+        });
+
+        it("Should not allow betting after closing", async () => {
+            await bet.connect(owner).close();
+
+            await expect(bet.connect(otherAccount).bet(optionIndex, { value: amount }))
+                .to.be.revertedWith("Betting is closed");
+        });
+
+        it("Should not allow betting on invalid option", async () => {
+            const invalidOptionIndex = 2;
+
+            await expect(bet.connect(otherAccount).bet(invalidOptionIndex, { value: amount }))
+                .to.be.revertedWith("Invalid option");
+        });
     });
 });
