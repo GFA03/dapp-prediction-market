24da65db52cc834db8d28a096016d01105556a3c
GFA03 <florinalexandru302003@gmail.com>
2024-11-13 17:45:40 +0200

    add functions for getting bets count and creating a bet

--- frontend/src/utils/contractServices.tsx
+++ frontend/src/utils/contractServices.tsx
@@ -1,5 +1,5 @@
 import Bet_Factory from "../artifacts/contracts/Bet_Factory.sol/Bet_Factory.json";
-import { BrowserProvider, Contract, parseEther, formatEther, JsonRpcSigner } from "ethers";
+import { BrowserProvider, Contract, JsonRpcSigner } from "ethers";
 import { CONTRACT_ADDRESS } from "./constants";
 import { MetaMaskInpageProvider } from "@metamask/providers";
 
@@ -37,3 +37,44 @@ export const requestAccount = async () => {
     return null;
   }
 };
+
+export const createBet = async (title: string, options: string[]) => {
+  if (!contract) {
+    await initialize();
+  }
+
+  try {
+    const tx = await contract!.createBet(title, options, { });
+    await tx.wait();
+  } catch (error: any) {
+    console.error("Error creating bet:", error.message);
+  }
+}
+
+export const getBets = async (limit: number, offset: number) => {
+  if (!contract) {
+    await initialize();
+  }
+
+  try {
+    const bets = await contract!.getBets(limit, offset);
+    return bets;
+  } catch (error: any) {
+    console.error("Error getting bets:", error.message);
+    return [];
+  }
+}
+
+export const getBetsCount = async () => {
+  if (!contract) {
+    await initialize();
+  }
+
+  try {
+    const length = await contract!.betsCount();
+    return length;
+  } catch (error: any) {
+    console.error("Error getting bets length:", error.message);
+    return 0;
+  }
+}
